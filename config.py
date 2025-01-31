412e16597b0947da28c46cda81757ae65146550b
mohanrc1515 <98520369+mohanrc1515@users.noreply.github.com>
2025-01-31 16:31:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,15 +2,12 @@ import os
 
 
 class Config(object):
-    API_HASH = os.environ.get("05c4cafe00b81ed83207bb4365e0053b")
-    BOT_TOKEN = os.environ.get("7696984863:AAEiUA76NTYiQ2dYlCzxEAaymT_FMnnkKpM")
-    TELEGRAM_API = os.environ.get("12655645")
-    OWNER = os.environ.get("1025922801")
-    OWNER_USERNAME = os.environ.get("mohanrc")
-    PASSWORD = os.environ.get("151515")
-    DATABASE_URL = os.environ.get("mongodb+srv://mohan:dNR0tiLpnTuTbiFu@cluster0.fhcly.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    LOGCHANNEL = os.environ.get("-1001714238387")  # Add channel id as -100 + Actual ID
-    GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID", "root")
-    USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", "None")
-    IS_PREMIUM = False
+    API_HASH = "05c4cafe00b81ed83207bb4365e0053b"
+    BOT_TOKEN = "7696984863:AAEiUA76NTYiQ2dYlCzxEAaymT_FMnnkKpM"
+    TELEGRAM_API = "12655645"
+    OWNER = "1025922801"
+    OWNER_USERNAME = "mohanrc"
+    PASSWORD = "151515"
+    DATABASE_URL = "mongodb+srv://mohan:dNR0tiLpnTuTbiFu@cluster0.fhcly.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    LOGCHANNEL = "-1001714238387"
     MODES = ["video-video", "video-audio", "video-subtitle", "extract-streams"]
