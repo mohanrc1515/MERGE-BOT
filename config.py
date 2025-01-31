7ac1e746f467c3a0dc1da632f364ea35e47b7fe4
mohanrc1515 <98520369+mohanrc1515@users.noreply.github.com>
2025-01-31 16:25:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,6 +11,6 @@ class Config(object):
     DATABASE_URL = os.environ.get("mongodb+srv://mohan:dNR0tiLpnTuTbiFu@cluster0.fhcly.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     LOGCHANNEL = os.environ.get("-1001714238387")  # Add channel id as -100 + Actual ID
     GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID", "root")
-    USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", None)
+    USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", "None")
     IS_PREMIUM = False
     MODES = ["video-video", "video-audio", "video-subtitle", "extract-streams"]
